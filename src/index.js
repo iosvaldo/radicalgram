72b007514b730e3f3b533a50f2584c518c870916
osvaldo <iosvaldo.app@gmail.com>
2022-11-26 17:48:25 -0500

    complete project ⚽️

---
+++ src/index.js
@@ -0,0 +1,59 @@
+// write your code here
+
+
+fetch("http://localhost:3000/images/1")
+  .then((res) => res.json())
+  .then((data) => renderContent(data));
+
+//create variables for our functions
+
+let renderTitle = document.getElementById("card-title");
+let renderImage = document.getElementById("card-image");
+let renderlikeBtn = document.getElementById("like-count");
+
+
+//create a function to render the title image and comments
+function renderContent(data) {
+  renderTitle.innerText = data.title;
+  renderImage.src = data.image;
+  renderlikeBtn.innerHTML = data.likes
+}
+
+// fetch request fot the comments
+fetch("http://localhost:3000/comments")
+.then((res) => res.json())
+.then((data) => renderComments(data));
+
+let renderComment = document.getElementById("comments-list");
+
+function renderComments(data) {
+  renderComment.innerHMTL = data.content;
+  for (const item of data) {
+    let newComment = document.createElement("li");
+    newComment.innerText = item.content;
+    renderComment.append(newComment);
+  }
+}
+
+//crete variable for the form
+
+mainForm = document.getElementById("comment-form");
+
+//Add and event listender for submission & render new comment
+mainForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let newInput = document.createElement("li");
+  newInput.innerText = comment.value;
+  renderComment.append(newInput);
+  e.target.reset();
+});
+
+let likeBtn = document.getElementById("like-button");
+//this is a variable for the like count field
+let num = 0;
+
+// addEventListener to incremment the click counter by one.
+likeBtn.addEventListener("click", function () {
+  num = num + 1;
+  renderlikeBtn.innerHTML = num + " likes";
+})
