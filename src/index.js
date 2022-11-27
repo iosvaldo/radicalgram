f4d403f4a1d5368464dd32d88af9ed4cfb4cc3da
osvaldo <iosvaldo.app@gmail.com>
2022-11-26 20:07:10 -0500

    update demo and readme file 🥋

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
 // write your code here
 
-
 fetch("http://localhost:3000/images/1")
   .then((res) => res.json())
   .then((data) => renderContent(data));
 
 //create variables for our functions
 
-let renderTitle = document.getElementById("card-title");
+let renderTitle = document.getElementById("card__title");
 let renderImage = document.getElementById("card-image");
 let renderlikeBtn = document.getElementById("like-count");
-
 
 //create a function to render the title image and comments
 function renderContent(data) {
   renderTitle.innerText = data.title;
   renderImage.src = data.image;
-  renderlikeBtn.innerHTML = data.likes
+  renderlikeBtn.innerHTML = data.likes;
 }
 
 // fetch request fot the comments
 fetch("http://localhost:3000/comments")
-.then((res) => res.json())
-.then((data) => renderComments(data));
+  .then((res) => res.json())
+  .then((data) => renderComments(data));
 
 let renderComment = document.getElementById("comments-list");
 
@@ -56,4 +54,14 @@ let num = 0;
 likeBtn.addEventListener("click", function () {
   num = num + 1;
   renderlikeBtn.innerHTML = num + " likes";
-})
+});
+
+let heart = document.getElementById("heart");
+heart.addEventListener("click", () => {
+  if (heart.classList.contains("red")) {
+    heart.classList.add("white");
+    heart.src = "assets/whiteHeart.png";
+  } else {
+    heart.src = "assets/redHeart.png";
+  }
+});
