2ab8ccb1c24d445150aca9a052247ed70f288998
osvaldo <iosvaldo.app@gmail.com>
2022-12-21 23:57:34 -0500

    refractor code local storage attempt 2 🧤

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-// write your code here
-fetch("http://localhost:3000/images/1")
-  .then((response) => response.json())
-  .then((data) => renderConent(data))
+// data variables
+const radicalgramAPI = "http://localhost:3000/images/1";
+const commentsAPI = "http://localhost:3000/comments";
 
+fetch(radicalgramAPI)
+  .then((response) => response.json())
+  .then(renderConent);
 
 //create variables for our functions
 let renderTitle = document.getElementById("card__title");
 let renderImage = document.getElementById("card-image");
 let renderLikeCount = document.getElementById("like-count");
-
 
 //create a function to render the title image and comments
 function renderConent(data) {
@@ -18,49 +19,79 @@ function renderConent(data) {
 }
 
 // fetch request fot the comments
-fetch("http://localhost:3000/comments")
+fetch(commentsAPI)
   .then((res) => res.json())
-  .then((data) => renderComments(data))
+  .then(renderComments);
 
 //create variable for comment
 let renderComment = document.getElementById("comments-list");
 
-//crete dunction to render comments & for loop.
+//create function to render comments & for loop.
 function renderComments(data) {
   for (const item of data) {
-    let newComment = document.createElement("li")
+    let newComment = document.createElement("li");
     newComment.innerHTML = item.content;
     renderComment.append(newComment);
   }
 }
 
 //create variable for the form
-
 mainform = document.getElementById("comment-form");
 
 //Add Event Listener to the form submission button
-
 mainform.addEventListener("submit", function (e) {
   e.preventDefault();
-  let newInput = document.createElement('li');
-  newInput.className = "stored-input"
+  let newInput = document.createElement("li");
+  newInput.className = "stored-input";
   newInput.innerHTML = comment.value;
   renderComment.append(newInput);
+  logKey();
   e.target.reset();
-})
+});
 
-// localStorage in use but need to clean it up. 
-const input = document.querySelector(".comment-input"),
-  h2 = document.querySelector(".heading_2");
+// localStorage in use but need to be clean it up.
+const input = document.querySelector("input"),
+li = document.querySelector(".h_3");
+li.innerHTML = localStorage.getItem("value");
+input.addEventListener("keyup",logKey);
 
-   h2.innerHTML = localStorage.getItem("value");
-
-input.addEventListener("keyup", display);
-  
-function display() {
-  // h2.innerText = input.value;
+function logKey() {
   localStorage.setItem("value", input.value);
-  // console.log();
- h2.innerHTML = localStorage.getItem("value");
-  
+  li.innerHTML = localStorage.getItem("value");
 }
+
+// second attempt at local storage
+// const commentForm = document.getElementById("comment-form");
+// const commentUl = document.getElementById("comment");
+// const commentInput = commentForm["comment"];
+
+// const comments = JSON.parse(localStorage.getItem("comments")) || [];
+
+// const addComment = (comment) => {
+//   comments.push({
+//     comment
+//   })
+//   localStorage.setItem("comments", JSON.stringify(comments))
+//   return { comment };
+// };
+
+// const createCommentElement = (comment) => {
+//   const commentDiv = document.createElement("div");
+//   const userComment = document.createElement("li");
+
+//   userComment.innerText = "Add a comment:" + comment;
+//   commentDiv.append(userComment);
+//   commentUl.appendChild(commentDiv);
+// };
+
+// comments.forEach(createCommentElement)
+
+// commentForm.onsubmit = e => {
+//   e.preventDefault();
+//   const newComment = addComment(
+//     commentInput.value
+//   );
+//   createCommentElement(newComment)
+
+//   commentInput.value = '';
+// };
